251525c08812fcc1e4bfbdcb4ada91f18c4f74d3
sid86-dev <sid86harth@gmail.com>
2021-09-15 11:12:38 +0530

    Piencrypt Test and example

---
+++ Test/pie_test.py
@@ -0,0 +1,70 @@
+from pie_clone import PiEncrypt
+import random
+import unittest
+
+
+Texts = ['Hi', 'Solo', 'Sid', 'Test', 'Lily', 'Python', 'Throttlerz', 'Password', 'Good Morning', 'Good Evening']   
+
+class TestClac(unittest.TestCase): 
+        def test_quantity(self):  
+            for i in range(800):
+                data = []
+                for i in range(5):
+                    r = random.choice(Texts)
+                    data.append(r)   
+                input = ", ".join(data) 
+                try:
+                    r = PiEncrypt('pic.png')
+                    r.get_data()
+                    r.hide_data(input)
+                    output = r.read_data()
+                    r.revert()
+                except Exception as e:
+                    raise ValueError(e)
+            return output
+
+        def test_quality(self):
+
+            for i in range(10):
+                data = []
+                for i in range(5000):
+                    r = random.choice(Texts)
+                    data.append(r)
+
+                input = ", ".join(data)
+
+                try:
+                    r = PiEncrypt('pic.png')
+                    r.get_data()
+                    r.hide_data(input)
+                    output = r.read_data()
+                    r.revert()
+                except Exception as e:
+                    raise ValueError(e)
+            return output
+
+
+def infile_test():
+    for i in range(10):
+        data = []
+        for i in range(5000):
+            r = random.choice(Texts)
+            data.append(r)
+  
+        input = ", ".join(data)
+        try:
+            r = PiEncrypt('pic.png')
+            r.get_data()
+            r.hide_data(input)
+            output = r.read_data()
+            r.revert()
+            print(output, end=r" %FINISHED% ")
+        except Exception as e:
+            raise ValueError(e)
+  
+if   __name__ == "__main__":
+    unittest.main()
+    # infile_test()
+
+
+
